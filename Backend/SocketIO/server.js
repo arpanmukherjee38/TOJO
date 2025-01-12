b652b0b534d3a93500708a3a645eacd2a9968aa4
arpanmukherjee38 <mukherjeearpan381@gmail.com>
2025-01-12 18:19:47 +0530

    Initial commit

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://tojo.onrender.com",
+    origin: "http://localhost:5002",
     methods: ["GET", "POST"],
   },
 });
