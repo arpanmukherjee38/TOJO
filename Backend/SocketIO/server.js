420a83d94fea464cf2d3a1275184ef9444d4d960
Arpan Mukherjee <mukherjeearpan381@gmail.com>
2025-01-12 17:07:34 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://tojo.onrender.com",
     methods: ["GET", "POST"],
   },
 });
